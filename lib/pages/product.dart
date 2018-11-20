a40106d64a24f475fe17f509a0d426081d03d0d4
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-20 14:14:40 +0200

    Add product details button and page

---
+++ lib/pages/product.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Product page'),
+      ),
+    );
+  }
+}
