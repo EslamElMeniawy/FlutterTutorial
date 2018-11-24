4157d4930aab666e57066569f8210617561f8a78
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-24 12:41:32 +0200

    Show modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,19 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create a product'),
+      child: RaisedButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Center(
+                child: Text('This is a Modal'),
+              );
+            },
+          );
+        },
+        child: Text('Save'),
+      ),
     );
   }
 }
