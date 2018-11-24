9072c70ee3af1514ef444d5150bc23a0c64d1f94
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-24 13:31:10 +0200

    Style text fields and add save button

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -8,21 +8,57 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String titleValue = '';
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          onChanged: (String value) {
-            setState(() {
-              titleValue = value;
-            });
-          },
-        ),
-        Text(titleValue),
-      ],
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Product Title',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 4,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              labelText: 'Product Description',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Product Price',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Save'),
+            color: Theme.of(context).primaryColor,
+            textColor: Colors.white,
+          )
+        ],
+      ),
     );
   }
 }
