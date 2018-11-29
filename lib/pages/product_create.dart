85a9e79dde57de631f8ca5f34fcf04e0d82f095c
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-29 13:26:42 +0200

    Submit form

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -12,9 +12,13 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String _titleValue;
-  String _descriptionValue;
-  double _priceValue;
+  final Map<String, dynamic> _formData = {
+    'title': null,
+    'description': null,
+    'price': null,
+    'image': 'assets/food.jpg'
+  };
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildTitleTextField() {
@@ -28,9 +32,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         }
       },
       onSaved: (String value) {
-        setState(() {
-          _titleValue = value;
-        });
+        _formData['title'] = value;
       },
     );
   }
@@ -48,9 +50,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         }
       },
       onSaved: (String value) {
-        setState(() {
-          _descriptionValue = value;
-        });
+        _formData['description'] = value;
       },
     );
   }
@@ -68,9 +68,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         }
       },
       onSaved: (String value) {
-        setState(() {
-          _priceValue = double.parse(value);
-        });
+        _formData['price'] = double.parse(value);
       },
     );
   }
@@ -78,15 +76,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   void _submitForm() {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-
-      final Map<String, dynamic> product = {
-        'title': _titleValue,
-        'description': _descriptionValue,
-        'price': _priceValue,
-        'image': 'assets/food.jpg'
-      };
-
-      widget.addProduct(product);
+      widget.addProduct(_formData);
       Navigator.pushReplacementNamed(context, '/products');
     }
   }
